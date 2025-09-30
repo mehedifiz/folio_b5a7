3834187d89eadf46d50387fd249110dd74ff9ab7
Mehedi Hasan Shanto <mehedi.sahnto.contract@gmail.com>
2025-09-30 06:31:31 -0700

    Hero section pushed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import { Button } from '@/components/ui/button';
+import Hero from '@/components/ui/hero';
 import React from 'react';
 
 const HomePage = () => {
   return (
     <div>
- <Button>df</Button>
-
- <div className='h-52 w-52 dark:bg-amber-800 bg-red-200'>
-
-
- </div>
+ <Hero/>
     </div>
   );
 };
